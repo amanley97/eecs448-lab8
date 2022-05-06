c959bdeacd9b7196ac01cc4caaa95ab1c6456aef
Alex Manley <98356915+amanley97@users.noreply.github.com>
2022-05-06 13:32:27 -0500

    Lab 10 commit

---
+++ lab_10/CreateUser.php
@@ -0,0 +1,43 @@
+<?php
+    error_reporting(E_ALL);
+    ini_set("display_errors", 1);
+    ini_set('display_startup_errors', 1);
+    echo "<html><head><title>Create User</title></head><body>";
+
+    function success() {
+        echo "<h1>Success</h1>";
+    }
+
+    // <!-- database credentials redacted -->
+        $mysqli = new mysqli("mysql.eecs.ku.edu", "USERNAME", "PASSWORD", "DATABASE"); 
+
+    echo "<html><body>";
+    if ($mysqli->connect_errno) {
+        echo "<h1>Could not connect to DB</h1></body></html>";
+        exit();
+    }
+    // Ensure nonempty string
+    if ($_POST["username"] == "") {
+        echo "<h1>Username cannot be empty</h1></body></html>";
+        exit();
+    }
+    // Prevent SQL injection by preparing and executing a statement
+    $stmt = $mysqli->prepare("INSERT INTO Users (user_id) VALUES (?);");
+    $stmt->bind_param("s", $_POST["username"]);
+    $result = $stmt->execute();
+
+    // SQL injection prone (multi_query is especially vulnerable)
+    // $query = "INSERT INTO Users (user_id) VALUES ('{$_POST['username']}')";
+    // e.g. $_POST["username"] = "ha'); TRUNCATE TABLE Users; INSERT INTO Users (user_id) VALUES ('pwned"
+    // $result = $mysqli->multi_query($query);
+    if ($result == false) {
+        echo "<h1>Insertion failed</h1>";
+    }
+    else {
+        success();
+    }
+
+    echo "</body></html>";
+    $mysqli->close();
+
+?>
---
+++ lab_10/ViewUsers.php
@@ -0,0 +1,27 @@
+<?php
+
+echo "<html><head><style>table, th, td {border: 1px solid black; border-collapse: collapse;}</style><title>View Users</title></head><body>";
+
+// <!-- database credentials redacted -->
+            $mysqli = new mysqli("mysql.eecs.ku.edu", "USERNAME", "PASSWORD", "DATABASE"); 
+if ($mysqli->connect_errno) {
+    echo "<h1>Could not connect to DB</h1></body></html>";
+    exit();
+}
+$query = "SELECT user_id FROM Users";
+
+if ($result = $mysqli->query($query)) {
+    echo "<table style=><thead><th>Users</th></thead><tbody>";
+    while ($row = $result->fetch_assoc()) {
+        printf("<tr><td>%s</td></tr>", $row["user_id"]);
+    }
+    echo "</tbody></table>";
+    $result->free();
+}
+else {
+    echo "<h1>Database query failed</h1>";
+}
+
+echo "</body></html>";
+$mysqli->close();
+?>
